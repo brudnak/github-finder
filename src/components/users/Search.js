f4e95915676a51d1ab7b0d3363c4012893efafb0
Andrew Brudnak <brudnak@icloud.com>
2019-06-06 13:49:06 -0700

    Set up clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: ''
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form onSubmit={onSubmit} className='form'>
+        <form onSubmit={this.onSubmit} className='form'>
           <input
             type='text'
             name='text'
@@ -28,6 +38,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
